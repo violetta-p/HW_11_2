ef6d31acc0cf3b5af7f049ae3687b3b67318da1c
ViolettaP <violetta.pukhova@mail.ru>
2023-05-13 20:32:25 +0300

    changed str_func (added capitalize_the_first and dogstring2)

--- str_func.py
+++ str_func.py
@@ -1,2 +1,13 @@
 def capitalize_all(word):
+    """
+    Перевод всех букв в верхний регистр
+    """
     return word.upper()
+
+
+def capitalize_the_first(word):
+    """
+    Перевод первой буквы каждого слова
+    в верхний регистр
+    """
+    return word.title()
